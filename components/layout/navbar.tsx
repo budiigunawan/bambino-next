db1c9f6f1fd92170f6533ec4d0192ac5a09e8b18
Budi Gunawan <budiindra.g@gmail.com>
2024-10-09 20:20:58 +0700

    feat: add navbar styling

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,5 +1,21 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import styles from './styles.module.scss';
 
 export default function Navbar() {
-  return <div className={styles.element}>Navbar</div>;
+  return (
+    <div className={styles.navbar}>
+      <nav className={styles.navbar__container}>
+        <Link href="/" className={styles.navbar__logo}>
+          <Image src="/logo.png" height={40} width={40} alt="Bambino logo" />
+          <p>Bambino</p>
+        </Link>
+        <ul className={styles.navbar__links}>
+          <li>
+            <Link href="/products">Products</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
