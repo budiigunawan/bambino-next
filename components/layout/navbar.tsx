880f6ec355a302513efa8cd8b98448dcb5f6bcd6
Budi Gunawan <budiindra.g@gmail.com>
2024-10-11 17:59:00 +0700

    feat: add counter on navbar

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,8 +1,19 @@
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from './styles.module.scss';
+import { IoCartOutline } from 'react-icons/io5';
+import { useCartStore } from '@/app/store';
+import { useMemo } from 'react';
 
 export default function Navbar() {
+  const { cart } = useCartStore((state) => state);
+  const cartItemCounter = useMemo(() => {
+    let counter = 0;
+    cart.forEach((item) => (counter += item.quantity));
+    return counter;
+  }, [cart]);
+
   return (
     <div className={styles.navbar}>
       <nav className={styles.navbar__container}>
@@ -10,11 +21,10 @@ export default function Navbar() {
           <Image src="/logo.png" height={40} width={40} alt="Bambino logo" />
           <p>Bambino</p>
         </Link>
-        {/* <ul className={styles.navbar__links}>
-          <li>
-            <Link href="/products">Products</Link>
-          </li>
-        </ul> */}
+        <Link href="/cart" className={styles.navbar__cart}>
+          <IoCartOutline fontSize={'24px'} />
+          <span className={styles.navbar__cart__badge}>{cartItemCounter}</span>
+        </Link>
       </nav>
     </div>
   );
