8cb363b67ffba641640a0b68977175fd8cbe9658
Budi Gunawan <budiindra.g@gmail.com>
2024-10-11 10:03:06 +0700

    feat: add product detail integration

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -1,97 +1,36 @@
+'use client';
+import { useParams } from 'next/navigation';
 import ProductDetail from '@/components/product/product-detail';
 import ProductRecommendation from '@/components/product/product-recommendation';
-
-const dummyData = {
-  id: 'clz70yukp00061145htqv02w7',
-  name: 'UT My Special Friends Lengan Pendek',
-  slug: 'ut-my-special-friends-lengan-pendek',
-  price: 149000,
-  imageUrl:
-    'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/466347/item/goods_62_466347.jpg',
-  stock: 100,
-  size: null,
-  sku: null,
-  overview: null,
-  materials: null,
-  categoryId: null,
-  createdAt: '2024-07-29T13:28:36.935Z',
-  updatedAt: '2024-07-29T13:28:36.935Z',
-};
-
-const recommendationsDummyData = [
-  {
-    id: 'clz70zdqv00071145nn8xlgli',
-    name: 'Baju Terusan | Lengan Pendek Garis',
-    slug: 'baju-terusan-lengan-pendek-garis',
-    price: 99000,
-    imageUrl:
-      'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/464513/item/goods_31_464513.jpg',
-    stock: 100,
-    size: null,
-    sku: null,
-    overview: null,
-    materials: null,
-    categoryId: null,
-    createdAt: '2024-07-29T13:29:01.782Z',
-    updatedAt: '2024-07-29T13:29:01.782Z',
-  },
-  {
-    id: 'clz70zu8800081145p7u1bivh',
-    name: 'Celana Legging Crop',
-    slug: 'celana-legging-crop',
-    price: 129000,
-    imageUrl:
-      'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/464633/item/goods_11_464633.jpg',
-    stock: 100,
-    size: null,
-    sku: null,
-    overview: null,
-    materials: null,
-    categoryId: null,
-    createdAt: '2024-07-29T13:29:23.142Z',
-    updatedAt: '2024-07-29T13:29:23.142Z',
-  },
-  {
-    id: 'clz71080p000911453rpjcsy1',
-    name: 'Baju Terusan Katun Mesh Tanpa Lengan Joy of Print',
-    slug: 'baju-terusan-katun-mesh-tanpa-lengan-joy-of-print',
-    price: 99000,
-    imageUrl:
-      'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/464576/item/goods_01_464576.jpg',
-    stock: 100,
-    size: null,
-    sku: null,
-    overview: null,
-    materials: null,
-    categoryId: null,
-    createdAt: '2024-07-29T13:29:41.016Z',
-    updatedAt: '2024-07-29T13:29:41.016Z',
-  },
-  {
-    id: 'clz710jj5000a1145364qubjl',
-    name: 'T-Shirt Kerah Bulat | Lengan Panjang',
-    slug: 't-shirt-kerah-bulat-lengan-panjang',
-    price: 149000,
-    imageUrl:
-      'https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/470873/item/goods_51_470873.jpg',
-    stock: 100,
-    size: null,
-    sku: null,
-    overview: null,
-    materials: null,
-    categoryId: null,
-    createdAt: '2024-07-29T13:29:55.938Z',
-    updatedAt: '2024-07-29T13:29:55.938Z',
-  },
-];
+import useFetch from '@/app/hooks/useFetch';
+import { useMemo } from 'react';
+import { Product } from '@/app/lib/types';
 
 export default function Page() {
+  const { slug } = useParams();
+
+  const { data: dataProduct, loading: loadingDetail } = useFetch(
+    `https://bambino-api.budigunawan.com/products/${slug}`
+  );
+  const { data: recommendationsData, loading: loadingRecommendation } =
+    useFetch('https://bambino-api.budigunawan.com/products?page=1&limit=5');
+
+  const productsRecommendation: Product[] = useMemo(() => {
+    if (recommendationsData?.products.length > 0) {
+      return recommendationsData.products.filter(
+        (product: Product) => product.slug !== slug
+      );
+    }
+
+    return [];
+  }, [recommendationsData, slug]);
+
   return (
     <>
-      <ProductDetail dataProduct={dummyData} />
+      <ProductDetail dataProduct={dataProduct} isLoading={loadingDetail} />
       <ProductRecommendation
-        products={recommendationsDummyData}
-        isLoading={false}
+        products={productsRecommendation}
+        isLoading={loadingRecommendation}
       />
     </>
   );
--- components/product/product-detail/index.tsx
+++ components/product/product-detail/index.tsx
@@ -7,76 +7,142 @@ import { Product } from '@/app/lib/types';
 
 type ProductDetailParams = {
   dataProduct: Product;
+  isLoading: boolean;
 };
 
-export default function ProductDetail({ dataProduct }: ProductDetailParams) {
+export default function ProductDetail({
+  dataProduct,
+  isLoading,
+}: ProductDetailParams) {
   return (
     <section className={styles.detail}>
       <div className={styles.detail__container}>
-        <Image
-          src={dataProduct.imageUrl}
-          width={0}
-          height={0}
-          sizes="100vw"
-          alt="children running"
-          style={{ width: '100%', height: 'auto' }}
-          className={styles.detail__container__image}
-        />
+        {isLoading ? (
+          <div
+            className={
+              styles.detail__container__image_skeleton +
+              ' skeleton skeleton-text'
+            }
+          ></div>
+        ) : (
+          <Image
+            src={dataProduct?.imageUrl}
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt="children running"
+            style={{ width: '100%', height: 'auto' }}
+            className={styles.detail__container__image}
+          />
+        )}
 
         <section className={styles.detail__container__main}>
           <section className={styles.detail__container__main__product}>
-            <h2>{dataProduct.name}</h2>
-            <p>{rupiahFormatter(dataProduct.price)}</p>
+            {isLoading ? (
+              <>
+                <h2>
+                  <div className="skeleton skeleton-text"></div>
+                  <div className="skeleton skeleton-text"></div>
+                  <div className="skeleton skeleton-text"></div>
+                </h2>
+                <p className="skeleton skeleton-text skeleton-header"></p>
+              </>
+            ) : (
+              <>
+                <h2>{dataProduct?.name}</h2>
+                <p>{rupiahFormatter(dataProduct?.price)}</p>
+              </>
+            )}
           </section>
 
           <section className={styles.detail__container__main__quantity}>
-            <p>Quantity</p>
-            <div className={styles.detail__container__main__quantity__input}>
-              <button>-</button>
-              <input type="number" name="quantity" id="quantity" />
-              <button>+</button>
-            </div>
-            <button
-              className={styles.detail__container__main__quantity__submit}
-            >
-              Add to cart
-            </button>
+            {isLoading ? (
+              <>
+                <p className="skeleton skeleton-text skeleton-header"></p>
+                <p className="skeleton skeleton-text skeleton-header"></p>
+                <div className="skeleton skeleton-text"></div>
+              </>
+            ) : (
+              <>
+                <p>Quantity</p>
+                <div
+                  className={styles.detail__container__main__quantity__input}
+                >
+                  <button>-</button>
+                  <input type="number" name="quantity" id="quantity" />
+                  <button>+</button>
+                </div>
+                <button
+                  className={styles.detail__container__main__quantity__submit}
+                >
+                  Add to cart
+                </button>
+              </>
+            )}
           </section>
 
           <section className={styles.detail__container__main__share}>
-            <p>Share</p>
-            <ul className={styles.detail__container__main__share__links}>
-              <li>
-                <a
-                  href={`https://twitter.com/share?url=https://evermos.budigunawan.com/products/${dataProduct.slug}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FaSquareXTwitter />
-                </a>
-              </li>
-              <li>
-                <a
-                  href={`http://facebook.com/share.php?u=https://evermos.budigunawan.com/products/${dataProduct.slug}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <FaSquareFacebook />
-                </a>
-              </li>
-            </ul>
+            {isLoading ? (
+              <>
+                <p className="skeleton skeleton-text skeleton-header"></p>
+                <p className="skeleton skeleton-text skeleton-header"></p>
+              </>
+            ) : (
+              <>
+                <p>Share</p>
+                <ul className={styles.detail__container__main__share__links}>
+                  <li>
+                    <a
+                      href={`https://twitter.com/share?url=https://evermos.budigunawan.com/products/${dataProduct?.slug}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <FaSquareXTwitter />
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href={`http://facebook.com/share.php?u=https://evermos.budigunawan.com/products/${dataProduct?.slug}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <FaSquareFacebook />
+                    </a>
+                  </li>
+                </ul>
+              </>
+            )}
           </section>
         </section>
 
         <section className={styles.detail__container__aside}>
-          <h3 className={styles.detail__container__aside__title}>Overview</h3>
-          <p className={styles.detail__container__aside__description}>
-            {dataProduct.overview ?? 'No overview information'}
-          </p>
-          <h3 className={styles.detail__container__aside__title}>Materials</h3>
-          <p className={styles.detail__container__aside__description}>
-            {dataProduct.materials ?? 'No materials information'}
-          </p>
+          {isLoading ? (
+            <>
+              <h3 className="skeleton skeleton-text skeleton-header"></h3>
+              <p className="skeleton skeleton-text"></p>
+              <p className="skeleton skeleton-text"></p>
+              <p className="skeleton skeleton-text"></p>
+              <h3 className="skeleton skeleton-text skeleton-header"></h3>
+              <p className="skeleton skeleton-text"></p>
+              <p className="skeleton skeleton-text"></p>
+              <p className="skeleton skeleton-text"></p>
+            </>
+          ) : (
+            <>
+              <h3 className={styles.detail__container__aside__title}>
+                Overview
+              </h3>
+              <p className={styles.detail__container__aside__description}>
+                {dataProduct?.overview ?? 'No overview information'}
+              </p>
+              <h3 className={styles.detail__container__aside__title}>
+                Materials
+              </h3>
+              <p className={styles.detail__container__aside__description}>
+                {dataProduct?.materials ?? 'No materials information'}
+              </p>
+            </>
+          )}
         </section>
       </div>
     </section>
